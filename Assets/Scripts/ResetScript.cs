ac6592ac4c6c0f6522e9895532d7920193b38c19
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2024-09-19 12:40:17 +0200

    Loading circle done

--- Assets/Scripts/ResetScript.cs
+++ Assets/Scripts/ResetScript.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ResetScript : MonoBehaviour
 {
-    float timeSinceAction;
+    private float timeSinceAction;
+    private Image resetCircle;
+    private float resetTime = 90f;
+    private float delay = 5f;
+    private void Start()
+    {
+        resetCircle = gameObject.transform.GetChild(0).GetComponentInChildren<Image>();
+    }
     void Update()
     {
         if (SceneManager.GetActiveScene().buildIndex != 0)
@@ -15,9 +23,11 @@ public class ResetScript : MonoBehaviour
             {
                 timeSinceAction = 0f;
             }
-            if (timeSinceAction >= 90f)
+            resetCircle.fillAmount = (timeSinceAction - delay) / resetTime;
+            if (timeSinceAction - delay >= resetTime)
             {
                 timeSinceAction = 0f;
+                resetCircle.fillAmount = 0f;
                 SceneManager.LoadScene(0);
             }
         }
