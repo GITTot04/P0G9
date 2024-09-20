8e2fc9367f4974bfcf98ef1fe38b9a7ad72bfce6
Purrplexe <piow375@gmail.com>
2024-09-20 12:06:55 +0200

    Made an answer screen and navigation
    Since we dont use proper structuring, Lazily making an answer screen on the page is most effective. Made it go to main screen and main screen to this multiple choice. can be changed in SceneManager ¯\_(ツ)_/¯

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,4 +10,8 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(sceneIndex);
     }
+    public void SwapScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
